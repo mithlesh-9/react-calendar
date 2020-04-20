538dca225ee6174e4f602bb0b757ed88d1bc6b32
mithleshfreak <mithlexh@gmail.com>
2020-04-20 18:15:19 +0545

    Week view has been added.

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -16,17 +16,21 @@ class Calendar extends Component {
         displayedEvents: [],
         displayRow: '',
         displayDay:'',
+        view:'week',
         events: [
-            {
-                start: moment('2020/04/1').format('l'),
-                badgeColor: 'red'
+            {   
+                id:1,
+                start: moment('2020-04-1').format("MM-DD-YYYY"),
+                badgeColor: 'purple'
             },
-            {
-                start: moment('2020/04/1').format('l'),
-                badgeColor: 'red'
+            {   
+                id:2,
+                start: moment('2020-04-1').format("MM-DD-YYYY"),
+                badgeColor: 'purple'
             },
-            {
-                start: moment().format('l')
+            {   
+                id:3,
+                start: moment().format("MM-DD-YYYY")
             },
         ]
     }
@@ -42,7 +46,7 @@ class Calendar extends Component {
     handleDrop = ({event,date}) => {
         console.log(date)
         this.setState(state => ({
-            events: [...state.events,{start:moment(date).format('l'),badgeColor:'purple'}]
+            events: [...state.events,{start:moment(date).format("MM-DD-YYYY"),badgeColor:'#0051ff',id:state.events.length + 1}]
         }))
     }
 
@@ -73,7 +77,7 @@ class Calendar extends Component {
 
     getEventsByDate = (date) => {
         const {events} = this.state        
-        return events.filter(event => moment(event.start).format('l') === date )
+        return events.filter(event => moment(event.start).format("MM-DD-YYYY") === moment(date).format("MM-DD-YYYY") )
     }
 
     daysInMonth = () => {
@@ -83,20 +87,20 @@ class Calendar extends Component {
         let daysinmonths = [];
         let d;
         for ( d = 1; d <= this.state.dateObject.daysInMonth(); d++) {
-            const fullDate = `${month}/${d}/${year}`
+            const fullDate = `${month}-${d}-${year}`
             const currentDay =  d;
             const events = this.getEventsByDate(fullDate)
-
+            
             const day = (
                         <td key={d} 
-                            className={`calendar-day ${fullDate === moment().format('l') ? 'today' : ''} ${displayDay === currentDay ? 'selected' : '' } ${events.length > 0 ? 'clickable' : ''}`} 
+                            className={`calendar-day ${fullDate === moment().format("MM-DD-YYYY") ? 'today' : ''} ${displayDay === currentDay ? 'selected' : '' } ${events.length > 0 ? 'clickable' : ''}`} 
                             onClick={() =>
                                 events.length > 0 
                                 ? this.displayEvents(events,`row-${this.getRowNumber(fullDate)}`,currentDay)
                                 : this.emptyFunction()
                                 }
-                                onDragOver={this.onDropFromOutside}
-                                onDrop={(e,date = new Date(fullDate)) => this.handleDrop({event:e,date})}
+                            onDragOver={this.onDropFromOutside}
+                            onDrop={(e,date = new Date(fullDate)) => this.handleDrop({event:e,date})}
                             
                         >
                             
@@ -106,14 +110,17 @@ class Calendar extends Component {
                             </small>
                             }
                             <span>{d}</span>
+                            {false &&
+                            <>
                             <div className="dots">
                             {events.length > 0 && events.map((event,i) => (
-                                <a key={`b${i}`} className="dot" style={{background:event.badgeColor ? event.badgeColor : 'purple'}}></a>
+                                <button key={`b${i}`} className="dot" style={{background:event.badgeColor ? event.badgeColor : '#0051ff'}}></button>
                             ))}
                             </div>
                             {events.length > 10 && (
                                 <div className="more-text">+ more...</div>
                             )}
+                            </>}
                         </td>
             )
             
@@ -122,6 +129,31 @@ class Calendar extends Component {
         }
         return daysinmonths
     }
+
+    daysInWeekView = () => {
+        return this.state.weekdaysShort.map((day,i) => {
+            const dayDate = this.getWeekDate(i);
+            const events = this.getEventsByDate(dayDate);
+            return (
+                <td 
+                  key={`${day}-${i}`}
+                  onDragOver={this.onDropFromOutside}
+                  onDrop={(e,date = new Date(dayDate)) => this.handleDrop({event:e,date})}
+                >
+                    {events.map(eventData => (
+                        <div 
+                            key={eventData.id} 
+                            className="week-event" 
+                            style={{background:eventData.badgeColor ? eventData.badgeColor : '#0051ff'}}
+                            onClick={(event)=>this.eventClicked({event,data:eventData})}
+                        >
+                            {eventData.start}
+                        </div>
+                    ))}
+                </td>
+        )})
+    }
+    
 
     displayEvents = (events,row,displayDay) => {
 
@@ -152,9 +184,11 @@ class Calendar extends Component {
         }))
     }
 
+    changeView = view => this.setState({view})
+
     getRowNumber = (numberDay) => {
         const blanks = this.renderBlanks().length;
-        const dayIncBlanks = blanks + Number(numberDay.split('/')[1])
+        const dayIncBlanks = blanks + Number(numberDay.split('-')[1])
 
         if(dayIncBlanks <= 7 ) {
             return 1;
@@ -171,6 +205,25 @@ class Calendar extends Component {
 
     eventClicked = e => {
         console.log(e)
+    }
+
+    getWeekDate = (day) => {
+        const { dateObject } = this.state;
+        const today = dateObject;
+        const weekNumber = today.week();
+        return moment(today.week(weekNumber).day(day).toDate()).format("MM-DD-YYYY")
+    }
+
+    onNextWeek = () => {
+        this.setState((state)=>({
+            dateObject: moment(state.dateObject).add(1,'weeks'),
+        }))
+    }
+
+    onPrevWeek = () => {
+        this.setState((state)=>({
+            dateObject: moment(state.dateObject).subtract(1,'weeks'),
+        }))
     }
 
 render() {
@@ -211,7 +264,7 @@ render() {
         {displayedEvents.length  > 0 && displayRow === `row-${i}` &&
         <tr>
             <td colSpan="7" className="events-display">
-                    {displayedEvents.map((event,i) => (<div key={`_${i}`} onClick={()=>this.eventClicked(event)} className="displayed-event"><a style={{backgroundColor: event.badgeColor ? event.badgeColor : 'purple' }} className="event-detail-badge"></a>{event.start}</div>))}
+                    {displayedEvents.map((eventData,i) => (<div key={eventData.id} onClick={(event)=>this.eventClicked({event,data:eventData})} className="displayed-event"><button style={{backgroundColor: eventData.badgeColor ? eventData.badgeColor : '#0051ff' }} className="event-detail-badge"></button>{eventData.start}</div>))}
             </td>
         </tr>
         }
@@ -219,42 +272,84 @@ render() {
         )} else return null;
     })
 
-    let renderTh = () => {
+    let renderDays = () => {
         return this.state.weekdaysShort.map(day => (
             <th key={day} className="week-day">
                 {day}
             </th>
         ))
     }
+    let renderWeekDays = () => {
+        const today = moment().format("MM-DD-YYYY")
+        return this.state.weekdaysShort.map((day,i) => (
+            <th key={day} className={`week-day${today === this.getWeekDate(i) ? ' today' : ''}`}>
+                <div>{day}</div>
+                <div className="date">{moment(this.getWeekDate(i)).format('Do MMM')}</div> 
+            </th>
+        ))
+    }
 
+    
 
-    const { months, dateObject} = this.state
+    const { months, dateObject, view} = this.state
     return (
         <div>
             <h2>Calendar</h2>
             <div draggable onDragStart={e => console.log(e,'hello')}>Hello</div>
             <div className="btns-group">
-                <button className="today-btn" onClick={this.setToday}>Today</button>
+                <button className="today-btn" onClick={this.setToday} style={{marginRight:'1em'}}>Today</button>
+                <button className={`today-btn inverted${view === 'month' ? ' selected': ''}`} onClick={()=>this.changeView('month')}>Month</button>
+                <button className={`today-btn inverted${view === 'week' ? ' selected': ''}`} onClick={()=>this.changeView('week')}>Week</button>
+                <button className={`today-btn inverted${view === 'day' ? ' selected': ''}`} onClick={()=>this.changeView('day')}>Day</button>
             </div>
+            {view === 'month' && 
+            <div>
             <div className="_heading">
                 <div>
-                <button onClick={()=>this.onPrev()}><img src={Prev} alt="prev" /></button>
+                <button onClick={()=>this.onPrev()} aria-label="Previous"><img src={Prev} alt="prev" /></button>
                 </div>
                     <div className="month-name">{`${months[dateObject.month()]}, ${dateObject.year()}`}</div>
                 <div>
-                <button onClick={()=>this.onNext()}><img src={Next} alt="next" /></button>
+                <button onClick={()=>this.onNext()} aria-label="Next"><img src={Next} alt="next" /></button>
                 </div>
             </div>
             <table border={'1px'} cellPadding={0}>
                 <thead>
                 <tr>
-                {renderTh()}
+                {renderDays()}
                 </tr>
                 </thead>
                 <tbody>
                 {daysinmonth}
                 </tbody>
             </table>
+            </div>}
+
+            {view === 'week' &&
+                <div>
+                <div className="_heading">
+                    <div>
+                    <button onClick={()=>this.onPrevWeek()}><img src={Prev} alt="prev" /></button>
+                    </div>
+                        <div className="month-name" style={{fontSize:'1.8em'}}>{`Week ${dateObject.week()} of ${dateObject.year()}`}</div>
+                    <div>
+                    <button onClick={()=>this.onNextWeek()}><img src={Next} alt="next" /></button>
+                    </div>
+                </div>
+                <table cellPadding={0}>
+                    <thead>
+                    <tr style={{border:'1px solid rgb(222, 222, 222)'}}>
+                    {renderWeekDays()}
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr style={{border:'1px solid rgb(222, 222, 222)',borderTop:'none'}}>
+                        {this.daysInWeekView()}
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            }
         </div>
       );
 }
